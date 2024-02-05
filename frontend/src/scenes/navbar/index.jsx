cb81daa52b192e766778c2b2b16345389790066a
Sonurxl423 <91655998+Sonurxl423@users.noreply.github.com>
2024-02-05 20:13:43 +0530

    Added the post features on the home page

--- frontend/src/scenes/navbar/index.jsx
+++ frontend/src/scenes/navbar/index.jsx
@@ -38,8 +38,7 @@ const NavPage = () => {
   const background = theme.palette.background.default;
   const primaryLight = theme.palette.primary.light;
   const alt = theme.palette.background.alt;
-  const fullName="Sonu Kumar";
-//   const fullName = `${user.firstName} ${user.lastName}`;
+  const fullName = `${user.firstName} ${user.lastName}`;
 
   return (
     <FlexBetween padding="1rem 6%" backgroundColor={alt}>
