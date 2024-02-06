db404b33879e3cd42a3298f099a898b530f64f9d
Sonurxl423 <91655998+Sonurxl423@users.noreply.github.com>
2024-02-06 08:29:49 +0530

    Approx all features are completed

--- frontend/src/scenes/homePage/index.jsx
+++ frontend/src/scenes/homePage/index.jsx
@@ -4,6 +4,8 @@ import { useSelector } from "react-redux";
 import UserWidget from "../widgets/UserWidget";
 import MyPostWidget from "../widgets/MyPostWidget";
 import PostsWidget from "../widgets/PostsWidget";
+import AdvertWidget from "../widgets/AdvertWidget";
+import FriendListWidget from "../widgets/FriendListWidget";
 
 const HomePage = () => {
   const isNonMobileScreens = useMediaQuery("(min-width:1000px");
@@ -27,9 +29,15 @@ const HomePage = () => {
           mt={isNonMobileScreens ? undefined : "2rem"}
         >
           <MyPostWidget picturePath={picturePath} />
-          <PostsWidget userId={_id}/>
+          <PostsWidget userId={_id} />
         </Box>
-        {isNonMobileScreens && <Box flexBasis="26%"></Box>}
+        {isNonMobileScreens && (
+          <Box flexBasis="26%">
+            <AdvertWidget />
+            <Box m="2rem 0" />
+            <FriendListWidget userId={_id}/>
+          </Box>
+        )}
       </Box>
     </Box>
   );
--- frontend/src/scenes/widgets/PostsWidget.jsx
+++ frontend/src/scenes/widgets/PostsWidget.jsx
@@ -22,7 +22,7 @@ const PostsWidget = ({ userId, isProfile = false }) => {
       `http://localhost:3001/posts/${userId}/posts`,
       {
         method: "GET",
-        headers: { Authorization: `Bearer ${token} `},
+        headers: { Authorization: `Bearer ${token} ` },
       }
     );
     const data = await response.json();
@@ -39,33 +39,33 @@ const PostsWidget = ({ userId, isProfile = false }) => {
 
   return (
     <>
-      {posts.map(
+    
+      {Array.isArray(posts) && posts.map(
         ({
-        _id,
-        userId,
-        firstName,
-        lastName,
-        description,
-        location,
-        picturePath,
-        userPicturePath,
-        likes,
-        comments,
-      })=>(
-        <PostWidget
-        key={_id}
-        postId={_id}
-        postUserId={userId}
-        name={`${firstName} ${lastName}`}
-        description={description}
-        location={location}
-        picturePath={picturePath}
-        userPicturePath={userPicturePath}
-        likes={likes}
-        comments={comments}
-        />
-      )
-      
+          _id,
+          userId,
+          firstName,
+          lastName,
+          description,
+          location,
+          picturePath,
+          userPicturePath,
+          likes,
+          comments,
+        }) => (
+          <PostWidget
+            key={_id}
+            postId={_id}
+            postUserId={userId}
+            name={`${firstName} ${lastName}`}
+            description={description}
+            location={location}
+            picturePath={picturePath}
+            userPicturePath={userPicturePath}
+            likes={likes}
+            comments={comments}
+          />
+        )
       )}
     </>
   );
